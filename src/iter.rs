9599a6c49c620bf7a047d77c99c1dddfd60ed944
Erlend Åmdal <erlend@aamdal.com>
2023-04-05 15:22:56 +0200

    SpatialFilterIter

--- src/iter.rs
+++ src/iter.rs
@@ -39,6 +39,52 @@ impl<'a, N, const D: usize, Value: Bounded<N, D>> Iterator for Iter<'a, N, D, Va
     }
 }
 
+// TODO: Is Value constraint necessary?
+pub trait SpatialFilter<N: Ord, const D: usize, Value: Bounded<N, D>> {
+    fn test_bounds(&self, bounds: &Bounds<N, D>) -> bool;
+    fn test_value(&self, value: &Value) -> bool;
+}
+
+pub struct SpatialFilterIter<'a, N: Ord, const D: usize, Value: Bounded<N, D>, Filter: SpatialFilter<N, D, Value>> {
+    pub(crate) filter: Filter,
+    pub(crate) tail: Vec<slice::Iter<'a, NodeRef<N, D, Value>>>,
+    pub(crate) head: slice::Iter<'a, Value>,
+}
+
+impl<'a, N: Ord, const D: usize, Value: Bounded<N, D>, Filter: SpatialFilter<N, D, Value>> Iterator
+    for SpatialFilterIter<'a, N, D, Value, Filter>
+{
+    type Item = &'a Value;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(value) = self
+                .head
+                .find(|value| self.filter.test_value(value))
+            {
+                return Some(value);
+            }
+
+            match self.tail.last_mut() {
+                Some(tail_iter) => {
+                    if let Some(intersecting) = tail_iter.find(|entry| self.filter.test_bounds(&entry.bounds)) {
+                        match &intersecting.node {
+                            Node::Inner(entries) => self.tail.push(entries.iter()),
+                            Node::Leaf(entries) => self.head = entries.iter(),
+                        }
+                    }
+                }
+                None => return Option::None,
+            }
+
+            // Clean up by removing empty iterators from the tail
+            while self.tail.last().map(|tail_iter| tail_iter.len()) == Some(0) {
+                self.tail.pop();
+            }
+        }
+    }
+}
+
 pub struct IntersectionIter<'a, N: Ord, const D: usize, Value: Bounded<N, D>> {
     pub(crate) bounds: Bounds<N, D>,
     pub(crate) tail: Vec<slice::Iter<'a, NodeRef<N, D, Value>>>,
